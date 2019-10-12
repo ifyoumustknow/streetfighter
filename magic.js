d1422c56f4deb9b8b5aa50fef045ce02c2d3a473
Rob Tang <ifyoumustknow@gmail.com>
2019-10-12 14:02:20 -0700

    revised js

--- magic.js
+++ magic.js
@@ -4,79 +4,91 @@
 // var chungHealth = 180;
 // var characters = []
 
-
 function start() {
     document.getElementById('logo').style.display = 'none';
     document.getElementById('start').style.display = 'none';
     document.getElementById('choose').style.display = 'inline';
 
 }
+
+window.onload = function () {
+    // $("#start").on("click", start)
+    $("#ryu").on("click", function () {
+        console.log("test");
+        // document.getElementById("P1-fighter").innerHTML = '<img src="assets/ryu_right.png"';
+        $("#choose").empty();
+        $("#title").empty();
+        $("#P1-fighter").empty();
+        $("#P1-fighter").append('<img src="assets/ryu_right.png" class="c-characters"/>');
+    });
+    $("#chung").on("click", function () {
+        console.log("test");
+        // document.getElementById("P1-fighter").innerHTML = '<img src="assets/ryu_right.png"';
+        $("#choose").empty();
+        $("#title").empty();
+        $("#P1-fighter").empty();
+        $("#P1-fighter").append('<img src="assets/chun_right.gif" class="c-characters"/>');
+    });
+    $("#zang").on("click", function () {
+        console.log("test");
+        // document.getElementById("P1-fighter").innerHTML = '<img src="assets/ryu_right.png"';
+        $("#choose").empty();
+        $("#title").empty();
+        $("#P1-fighter").empty();
+        $("#P1-fighter").append('<img src="assets/zang_right.png" class="c-characters"/>');
+    });
+    $("#honda").on("click", function () {
+        console.log("test");
+        // document.getElementById("P1-fighter").innerHTML = '<img src="assets/ryu_right.png"';
+        $("#choose").empty();
+        $("#title").empty();
+        $("#P1-fighter").empty();
+        $("#P1-fighter").append('<img src="assets/Honda_right.png" class="c-characters"/>');
+    });
+};
+
+
+
 //computer random choose character function// **in progress**
 
 // function cpuSelect() {
-//     var cpufighters = [
-//         { name: 'chun', image: 'banana.jpg' }, 
-//         { name: 'honda', image: 'apple.jpg' }, 
-//         { name: 'zang', image: 'orange.jpg' }
-//         { name: 'ryu', image: 'orange.jpg' } 
-//       ];
-//       for (var i = 0; i < boldStuffs.length; i += 1) {
-//         var cpuselect = getRandomItem(cpufighters);
-//         boldStuffs[i].innerHTML = <img src="'+fruit.image+'">';
+//     var cpuFighters = [
+//         { name: 'chun'(<img src="assets/ryu_right.png" class="c-characters" />) },
+//         { name: 'honda'(<img src="assets/ryu_right.png" class="c-characters" />) },
+//         { name: 'zang'(<img src="assets/ryu_right.png" class="c-characters" />) }
+//         { name: 'ryu'(<img src="assets/ryu_right.png" class="c-characters" />) }
+//     ];
+
+//     for (var i = 0; i < cpuFighters.length; i += 1) {
+//         $("#P1-fighter").append('<img src=>');
+
 //     }
 // }
 
+/*
 
-//player chooses character fucntion/// **in progress**
-function chun() {
-    document.getElementById('logo').style.display = 'none';
-    document.getElementById('start').style.display = 'none';
-    document.getElementById('choose').style.display = 'none';
-    document.getElementById('chun-rht').style.display = 'inline';
-}
+for (let i = 0; i < cpufighters.length; i++) {
+    const image = cpufighters[i].image;
+            $("#test-image" + i + 1).attr("src", image);
+        }
 
-function honda() {
-    document.getElementById('logo').style.display = 'none';
-    document.getElementById('start').style.display = 'none';
-    document.getElementById('choose').style.display = 'none';
-    document.getElementById('honda-rht').style.display = 'inline';
-}
+        */
 
-function zang() {
-    document.getElementById('logo').style.display = 'none';
-    document.getElementById('start').style.display = 'none';
-    document.getElementById('choose').style.display = 'none';
-    document.getElementById('zang-rht').style.display = 'inline';
-}
+        // function to simply above and remove previous player// **in progress**
 
-function ryu() {
-    document.getElementById('logo').style.display = 'none';
-    document.getElementById('start').style.display = 'none';
-    document.getElementById('choose').style.display = 'none';
-    document.getElementById('ryu-rht').style.display = 'inline';
-}
+        // function to calculate random attack points to "Special" "Kick" "Punch" buttons//
 
-// function to simply above and remove previous player// **in progress**
+        // function to calculate randon counter attack points to "Special" "Kick" "Punch" buttons//
 
-//     if
-//     document.getElementById('logo').style.display = 'none';
-//     document.getElementById('start').style.display = 'none';
-//     document.getElementById('ryu-rht').style.display = 'inline';
-// }
-
-// function to calculate random attack points to "Special" "Kick" "Punch" buttons//
-
-// function to calculate randon counter attack points to "Special" "Kick" "Punch" buttons//
-
-// function to display attack and counter attack points//
+        // function to display attack and counter attack points//
 
 
-// function to play again//
+        // function to play again//
 
-// BONUS function to track health points using health bar ///
+        // BONUS function to track health points using health bar ///
 
-// let cpuhealth = document.getElementById("cpuhealth")
-// health.value -= 10; //Or whatever you want to do with it.
+        // let cpuhealth = document.getElementById("cpuhealth")
+        // health.value -= 10; //Or whatever you want to do with it.
 
-// let p1health = document.getElementById("p1health")
-// health.value -= 10; //Or whatever you want to do with it.
+        // let p1health = document.getElementById("p1health")
+        // health.value -= 10; //Or whatever you want to do with it.
