529402d3e02663b3f5471f412dada4beffb51904
Rob Tang <ifyoumustknow@gmail.com>
2019-10-12 00:02:16 -0700

    updated code

--- magic.js
+++ magic.js
@@ -6,6 +6,71 @@
 
 
 function start() {
-
+    document.getElementById('logo').style.display = 'none';
+    document.getElementById('start').style.display = 'none';
+    document.getElementById('choose').style.display = 'inline';
 
 }
+//computer random choose character function// **in progress**
+
+// function cpuSelect() {
+//     var cpufighters = [
+//         { name: 'chun', image: 'banana.jpg' }, 
+//         { name: 'honda', image: 'apple.jpg' }, 
+//         { name: 'zang', image: 'orange.jpg' }
+//         { name: 'ryu', image: 'orange.jpg' } 
+//       ];
+//       for (var i = 0; i < boldStuffs.length; i += 1) {
+//         var cpuselect = getRandomItem(cpufighters);
+//         boldStuffs[i].innerHTML = <img src="'+fruit.image+'">';
+//     }
+// }
+
+
+//player chooses character fucntion/// **in progress**
+function chun() {
+    document.getElementById('logo').style.display = 'none';
+    document.getElementById('start').style.display = 'none';
+    document.getElementById('choose').style.display = 'none';
+    document.getElementById('chun-rht').style.display = 'inline';
+}
+
+function honda() {
+    document.getElementById('logo').style.display = 'none';
+    document.getElementById('start').style.display = 'none';
+    document.getElementById('choose').style.display = 'none';
+    document.getElementById('honda-rht').style.display = 'inline';
+}
+
+function zang() {
+    document.getElementById('logo').style.display = 'none';
+    document.getElementById('start').style.display = 'none';
+    document.getElementById('choose').style.display = 'none';
+    document.getElementById('zang-rht').style.display = 'inline';
+}
+
+function ryu() {
+    document.getElementById('logo').style.display = 'none';
+    document.getElementById('start').style.display = 'none';
+    document.getElementById('choose').style.display = 'none';
+    document.getElementById('ryu-rht').style.display = 'inline';
+}
+
+// function to simply above and remove previous player// **in progress**
+
+//     if
+//     document.getElementById('logo').style.display = 'none';
+//     document.getElementById('start').style.display = 'none';
+//     document.getElementById('ryu-rht').style.display = 'inline';
+// }
+
+// function to calculate random attack points to "Special" "Kick" "Punch" buttons//
+
+// function to calculate randon counter attack points to "Special" "Kick" "Punch" buttons//
+
+// function to display attack and counter attack points//
+
+
+// function to play again//
+
+// BONUS function to track health points using health bar ///
